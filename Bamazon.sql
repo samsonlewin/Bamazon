61798d6717ed9a7c63b2f08e4dc81579b5ff5f5b
Samson Eitan Lewin <samsoneitanlewin@MacBook-Pro-de-Samson.local>
2017-04-16 18:03:26 -0700

    still need to handle display since inquirer too fast

---
+++ Bamazon.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE Bamazon;
+
+USE Bamazon;
+
+CREATE TABLE products (
+item_id INT AUTO_INCREMENT NOT NULL,
+product_name VARCHAR(50) NOT NULL, 
+department_name VARCHAR(50) NOT NULL, 
+price DECIMAL(19,4) NOT NULL,
+stock_quantity INTEGER(30),
+PRIMARY KEY (item_id)
+);
+
+
+INSERT INTO products (product_name,department_name,price,stock_quantity )
+VALUES ("Milk Cholocate", "Groceries", 4.5, 5000);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity )
+VALUES ("Small wooden table", "Furniture",250, 100);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity )
+VALUES ("Bunk beds", "Furniture",820.99, 10);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity )
+VALUES ("Painting of a cat", "Art",10009.76, 1);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity )
+VALUES ("Bananas", "Groceries",1.99, 10000);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity )
+VALUES ("Apples", "Groceries",0.99, 20000);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity )
+VALUES ("Apple TV", "Tech",100.89, 20);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity )
+VALUES ("Google Home", "Tech",120.59, 30);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity )
+VALUES ("Cooking Book", "Kitchen",20.29, 35);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity )
+VALUES ("Rice Cooker", "Kitchen",53.66, 3);
+
+SELECT * FROM products;
+
---
+++ bamazonCustomer.js
@@ -0,0 +1,72 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var selection =[];
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "Bamazon"
+});
+
+
+buyProduct();
+
+
+//display all of the items available for sale. Include the ids, names, and prices of products for sale.
+function displayAll (){
+	connection.query('SELECT item_id, product_name, price FROM products', function(err,res){
+	if (err) throw err;
+	for (var i=0; i<res.length;i++){
+		console.log("id: " + res[i].item_id + " name: "+ res[i].product_name + " price: "+ res[i].price);
+	};
+});
+
+};
+
+
+//prompt users with two messages.
+//ask them the ID of the product they would like to buy
+//ask how many units of the product they would like to buy
+function buyProduct (){
+	displayAll();
+	inquirer.prompt({
+		type : "input",
+		name : "id",
+		message : "Select the item id you would like to buy"
+	}).then(function(answer){
+		connection.query('SELECT * FROM products WHERE ?',{ item_id: answer.id}, function(err, res){
+		if (err) throw err;
+		var theItem = answer.id;
+		var quantity = res[0].stock_quantity;
+		inquirer.prompt({
+					type : "input",
+					name : "units",
+					message : "how many units would you like to buy?"
+				}).then(function(answer){
+					if (answer.units > quantity){
+						console.log("Insufficient quantity!");
+						buyProduct();
+					}else{
+						connection.query("UPDATE products SET ? WHERE ?", [{
+  						stock_quantity: res[0].stock_quantity-answer.units
+						}, {
+  						item_id: theItem
+						}],function(err,res){});
+
+						console.log("The total cost of your pruchase will be $"+answer.units*res[0].price +". Thanks for buying at Bamazon!");
+  
+					};
+
+				});
+
+		
+		});
+	});
+};
+
